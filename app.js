77223b8931430fe48ffc9ae40feaf8e04268442d
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-08-15 09:33:46 -0500

    finished 9.2

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const generatePage = (userName, githubName) => {
-    return `
-      Name: ${userName}
-      GitHub: ${githubName}
-    `;
-  };
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-  console.log(generatePage('Jane', 'janehub'));
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
